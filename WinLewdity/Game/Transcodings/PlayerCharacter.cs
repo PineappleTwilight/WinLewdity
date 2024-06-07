fbcd92d766269c9efeb1324cd66db2bc23a88c17
Jeffrey Baily <jeffbaily@msn.com>
2024-06-06 23:59:00 -0400

    Foundations for player stats

---
+++ WinLewdity/Game/Transcodings/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinLewdity_GUI.Game.Transcodings
+{
+    public class PlayerCharacter
+    {
+        // General stats
+        public float Pain { get; set; }
+
+        public float Arousal { get; set; }
+        public float Fatigue { get; set; }
+        public float Stress { get; set; }
+        public float Trauma { get; set; }
+        public float Control { get; set; }
+        public float Allure { get; set; }
+    }
+}
