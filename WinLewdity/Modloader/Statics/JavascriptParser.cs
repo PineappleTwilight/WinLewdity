4f062e4ac84b2dfc5f6509a2ca6eac899d119cba
Jeffrey Baily <jbaily@msn.com>
2024-06-21 23:58:13 -0400

    Minor function additions

--- WinLewdity/Modloader/Statics/JavascriptParser.cs
+++ WinLewdity/Modloader/Statics/JavascriptParser.cs
@@ -8,13 +8,31 @@ using System.Threading.Tasks;
 
 namespace WinLewdity_GUI.Modloader.Statics
 {
+    /// <summary>
+    /// Class that holds various functions related to AST.
+    /// </summary>
     public static class JavascriptParser
     {
         private static Parser parser = new Parser();
 
-        public static Script ParseScript(string script)
+        /// <summary>
+        /// Parses raw JS code into AST.
+        /// </summary>
+        /// <param name="script"></param>
+        /// <returns></returns>
+        public static Script ParseJavascript(string script)
         {
             return parser.ParseScript(script);
+        }
+
+        /// <summary>
+        /// Parses compiled AST into raw JS code.
+        /// </summary>
+        /// <param name="script"></param>
+        /// <returns></returns>
+        public static string ParseAST(Script script)
+        {
+            return script.ToJavaScript();
         }
     }
 }
